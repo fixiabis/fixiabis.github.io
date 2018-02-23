d396aad42558e9a1e249a62e3b359578de72399a
fixiabis <benkao0000@gmail.com>
2018-02-23 12:10:41 +0800

    visualize temp and var

--- js-visualizer/js/step.js
+++ js-visualizer/js/step.js
@@ -1,4 +1,6 @@
-var step = 1;
+var step = 1, 
+	logger = document.querySelector(".log"),
+	temp = document.querySelector(".temp");
 (function nextStep() {
 	var nowStep = document.querySelectorAll(`[data-step~=s${step}]`),
 		othStep = document.querySelectorAll(`[data-step]:not([data-step~=s${step}])`);
@@ -7,6 +9,8 @@ var step = 1;
 	if (!nowStep.length) return;
 	for (var i = 0; i < nowStep.length; i++)
 		nowStep[i].classList.add("now");
+	temp.innerHTML = "";
+	if (action && action[step]) action[step]();
 	step++;
 	setTimeout(nextStep, 1500);
 })();
